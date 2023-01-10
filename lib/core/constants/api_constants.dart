54351435e2ab6fe282d9064d3f563409a056f0a4
Xuan Canh <xuancanhit99@gmail.com>
2023-01-11 01:10:39 +0300

    Initial commit

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const apiKey = 'd1aa58313f58aa9537e9aba1429fabf5';
+  static const fiveDaysWeatherApi =
+      'https://api.openweathermap.org/data/2.5/forecast?q=moscow&appid=d1aa58313f58aa9537e9aba1429fabf5';
+  static const currentWeatherApi =
+      'https://api.openweathermap.org/data/2.5/weather?q=moscowappid=d1aa58313f58aa9537e9aba1429fabf5&';
+}
